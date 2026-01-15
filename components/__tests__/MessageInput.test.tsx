48ac9ce27766258da052e735afbca41611c54c10
POWCHU\Arwin Delasan <delasanarwin@gmail.com>
2026-01-15 23:57:06 +0800

    message input text type-check fixes

--- components/__tests__/MessageInput.test.tsx
+++ components/__tests__/MessageInput.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { MessageInput } from '../messaging/MessageInput';
 import type { ConversationParticipant } from '@/lib/supabase/messaging';
 
